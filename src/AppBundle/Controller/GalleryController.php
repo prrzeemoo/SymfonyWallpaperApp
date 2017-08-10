b0a2f7ac5931884687641a999cff80234a312838
prrzeemoo <przemekstopka@gmail.com>
2017-08-10 19:31:43 +0200

    Wyswietlanie Obrazow Kontroler oraz Widok

---
+++ src/AppBundle/Controller/GalleryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GalleryController extends Controller
+{
+    /**
+     * @Route("/gallery", name="gallery")
+     */
+    public function indexAction()
+    {
+        return $this->render('gallery/index.html.twig', [
+            'images' => [
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+                'image_1.jpg',
+            ]
+        ]);
+    }
+}
